c157037b87ce34afa85db9170496bcd8dfecc37a
Anderson <anderson@rech.com.br>
2018-09-14 16:24:48 -0300

    Início renderização dos contatos

--- App.js
+++ App.js
@@ -3,10 +3,32 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header.js';  
 
 export default class App extends React.Component {
+  renderList() {
+    const names = [
+      'Anderson Hansen',
+      'Anderson Hansen',
+      'Anderson Hansen'
+    ];
+
+    const textElements = names.map(nome => {
+      return <Text> { nome } </Text>
+
+    })
+    
+    return textElements;
+    
+
+
+
+
+  }
   render() {
     return (
       <View>
-        <Header title="Contatos"/>        
+        <Header title="Contatos"/>
+        
+        {/* É usado this por que declaramos a função dentro da própria classe */}
+        { this.renderList() }        
       </View>
     );
   }
